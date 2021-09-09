39b8d768f99b9225979a93a8063ad37a7eba1dd2
Merfrei <contact@merfrei.com>
2021-09-09 17:17:48 -0400

    Current user show in top menu and logout implementation

--- src/app/_models/user.model.ts
+++ src/app/_models/user.model.ts
@@ -2,5 +2,4 @@ export interface User {
   email: string;
   name: string;
   isAdmin: boolean;
-  jwtToken?: string;
 }
--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -1,14 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { Subscription } from 'rxjs';
+import { User } from '../_models/user.model';
+import { AuthService } from './../_services/auth.service';
+
+const GET_USER = gql`
+  query GetUser {
+    loggedUser {
+      email
+      name
+      isAdmin
+    }
+  }
+`;
 
 @Component({
   selector: 'app-top-menu',
   templateUrl: './top-menu.component.html',
   styleUrls: ['./top-menu.component.css'],
 })
-export class TopMenuComponent implements OnInit {
+export class TopMenuComponent implements OnInit, OnDestroy {
   public isMenuCollapsed = true;
+  public user?: User;
+  public loading = false;
 
-  constructor() {}
+  private querySubscription?: Subscription;
 
-  ngOnInit(): void {}
+  constructor(
+    private readonly apollo: Apollo,
+    private readonly authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.querySubscription = this.apollo
+      .watchQuery<any>({
+        query: GET_USER,
+      })
+      .valueChanges.subscribe(({ data, loading }) => {
+        this.loading = loading;
+        this.user = data.loggedUser;
+      });
+  }
+
+  ngOnDestroy() {
+    this.querySubscription?.unsubscribe();
+  }
+
+  userLogout() {
+    this.authService.logout();
+  }
 }
